5a0cfa91839c9d653e1eb87c8a6ad16ea8e73fe5
Santiago Betancur <santyagobetancurth2004@gmail.com>
2024-05-01 17:32:14 -0500

    lastest version

--- editPage/static/scripts/editPage.js
+++ editPage/static/scripts/editPage.js
@@ -2,7 +2,7 @@ function zoomIn() {
     var div = document.getElementById('vacancy-container');
     var width = div.offsetWidth;
     var height = div.offsetHeight;
-    div.style.width = (width * 1.1) + 'px'; // Aumenta el tamaño en un 10%
+    div.style.width = (width * 1.1) + 'px'; 
     div.style.height = (height * 1.1) + 'px';
   }
   
@@ -10,21 +10,26 @@ function zoomIn() {
     var div = document.getElementById('vacancy-container');
     var width = div.offsetWidth;
     var height = div.offsetHeight;
-    div.style.width = (width / 1.1) + 'px'; // Reduce el tamaño en un 10%
+    div.style.width = (width / 1.1) + 'px';
     div.style.height = (height / 1.1) + 'px';
   }
   
   function progressIn(step){
-    var div = document.getElementById('progress');
-    var width = div.offsetWidth;
-  
-    
-    
+    var div = document.getElementById('prBar');
+    div.style.transition = 'width 0.5s ease-in-out';
+
+    setTimeout(function() {
+
+      switch (step){
+        case 1: div.style.width = 33.33 + '%'; break;
+        case 2: div.style.width = 66.66 + '%'; break;
+        case 3: div.style.width = 100 + '%'; break;
+        default: div.style.width = 0 + '%'; break;
+      }
+        
+    }, 300); 
+
 
     
-  }
-  function progressOut(){
-    var div = document.getElementById('progress');
-    var width = div.offsetWidth;
-   
+    
   }
--- editPage/views.py
+++ editPage/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def step1(request):
+    context = {}
     if (request.method == 'POST'):
+        
         return redirect('paso2/')
-
-    return render(request, 'step1.html')
+    context['current_step'] = 1
+    context['step_text'] = 'Información básica'
+    return render(request, 'step1.html', context)
 
 def step2(request):
-    return render(request, 'step2.html')
+    context = {}
+    context['current_step'] = 2
+    context['step_text'] = 'Personalización'
+    return render(request, 'step2.html', context)

a11dc5da39f71592b4b95fa07243bdb7da3a6e1f
M4C4NO5 <stufontrabajo@gmail.com>
2024-05-13 10:41:15 -0500

    Feature: Trying fix some mistakes

--- editPage/views.py
+++ editPage/views.py
@@ -9,22 +9,36 @@ from plantilla.models import TemplateModel
 
 
 def step1(request):
-    context = {}
-    savedTemplate = TemplateModel.objects.latest('id')
-    default_name = savedTemplate.name
-    default_title = savedTemplate.title
-    default_desc = savedTemplate.desc
-    default_color = savedTemplate.color
-    default_url = savedTemplate.url
+    if not TemplateModel.objects.exists():
+        basis = TemplateModel(name="prueba",title="prueba",desc="prueba",color="prueba",url="prueba")
+        basis.save()
+    if TemplateModel.objects.exists():
+        context = {}
+        savedTemplate = TemplateModel.objects.latest('id')
+        default_name = savedTemplate.name
+        default_title = savedTemplate.title
+        default_desc = savedTemplate.desc
+        default_color = savedTemplate.color
+        default_url = savedTemplate.url
 
     baseTemplate = Template(default_name,default_title,default_desc,default_url,default_color)
 
     editableTemplate = baseTemplate.clone()
     if (request.method == 'POST'):
+        context["title"]=request.POST["title"]
+        context["desc"]=request.POST["desc"]
+        context["url"]=request.POST["url"]
+
+        title = context["title"]
+        desc = context["desc"]
+        url = context["url"]
         
-        
+        editableTemplate.title = title
+        editableTemplate.desc = desc
+        editableTemplate.url = url
+
         return redirect('paso2/')
-    
+
 
     context['current_step'] = 1
     context['step_text'] = 'Información básica'
@@ -35,4 +49,14 @@ def step2(request):
     context = {}
     context['current_step'] = 2
     context['step_text'] = 'Personalización'
+
+    #Getting data from step 1
+    if 'title' in request.POST:
+        context['title']= request.POST.get('title','')
+        prueba = context['title']
+        print(prueba)
+    if 'desc' in request.POST:
+        context['desc']= request.POST.get('desc','')
+    if 'url' in request.POST:
+        context['url']= request.POST.get('url','')
     return render(request, 'step2.html', context)
--- plantilla/models.py
+++ plantilla/models.py
@@ -5,13 +5,9 @@ from django.utils import timezone
 
 # Create your models here.
 class TemplateModel(models.Model):
+    id=models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     title = models.CharField(max_length=30, null=False, default='N/A')
     desc = models.CharField(max_length=40, null=False, default='N/A')
     url = models.URLField(max_length=100, null=True)
     color = models.CharField(max_length=30, null=False, default='N/A')
-
-
-    
-    
-    

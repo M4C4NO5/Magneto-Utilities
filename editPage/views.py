32ee1f68c54eb9e8390a137d767cd82f23a4a38b
CursedDani <dfarangog@gmail.com>
2024-05-16 04:32:57 -0500

    Fix: changes on template style stay after changes

--- editPage/views.py
+++ editPage/views.py
@@ -1,31 +1,46 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from plantilla.views import Template
 from plantilla.models import TemplateModel
-from django.contrib.sessions.models import Session
+
 
 # Create your views here.
 
 # Template object clone
 
 
-def template_to_edit():
+def template_to_edit(request):
    
     if TemplateModel.objects.exists():
         savedTemplate = TemplateModel.objects.latest('id')
         default_name = savedTemplate.name
         default_title = savedTemplate.title
         default_desc = savedTemplate.desc
-        default_color = savedTemplate.color
+        if 'color' in request.session:
+            default_color = request.session['color']
+        else:
+            default_color = savedTemplate.color
         default_url = savedTemplate.url
-        default_font = savedTemplate.font
-        default_font_color = savedTemplate.font_color
+        if 'font' in request.session:
+            default_font = request.session['font']
+        else:
+            default_font = savedTemplate.font
+        if 'font_color' in request.session:
+            default_font_color = request.session['font_color']
+        else:
+            default_font_color = savedTemplate.font_color
 
     baseTemplate = Template(default_name,default_title,default_desc,default_url,default_color, default_font, default_font_color)
     return baseTemplate.clone()
     
 def step1(request):
     context = {}
-    template = template_to_edit()
+    if 'font' in request.session:
+        del request.session['font']
+    if 'font_color' in request.session:
+        del request.session['font_color']
+    if 'color' in request.session:
+        del request.session['color']
+    template = template_to_edit(request)
     context['template'] = template 
 
     if (request.method == 'POST'):
@@ -41,7 +56,7 @@ def step1(request):
     return render(request, 'step1.html', context)
 
 def step2(request):
-    template = template_to_edit()
+    template = template_to_edit(request)
     context = {}
     context['current_step'] = 2
     context['step_text'] = 'Personalización'
@@ -61,18 +76,21 @@ def step2(request):
   
             template.font = font
             context['template'] = template
+            request.session['font'] = font
             return render(request, 'step2.html', context)
         if 'change-font-color' in request.POST:
 
             font_color = request.POST['change-font-color']
             template.font_color = font_color
             context['template'] = template
+            request.session['font_color'] = font_color
             return render(request, 'step2.html', context)
         
         if 'change-color' in request.POST:
             color = request.POST['change-color']
             template.color = color
             context['template'] = template
+            request.session['color'] = color
             return render(request, 'step2.html', context)
         
         if 'next-step' in request.POST:
